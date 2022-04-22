ca5e1a28df914008900fde2459f79c26d09ca2f5
qowldud95 <girlwldud@naver.com>
2022-04-22 17:24:00 +0900

    20220422

---
+++ scheduling/src/main/java/jiyoung/scheduling/controller/schedulingController.java
@@ -0,0 +1,8 @@
+package jiyoung.scheduling.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class schedulingController {
+}
---
+++ scheduling/src/main/java/jiyoung/scheduling/service/DynamicChangeScheduler.java
@@ -0,0 +1,20 @@
+package jiyoung.scheduling.service;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DynamicChangeScheduler {
+    private ThreadPoolTaskScheduler scheduler;
+    private String con = "*/2 * * * * *";
+
+    public void startScheduler(){
+        scheduler = new ThreadPoolTaskScheduler();
+        scheduler.initialize();  //scheduler 초기화
+        scheduler.schedule(getRunnable(), getTrigger());     //scheduler setting
+    }
+
+    private Runnable getRunnable(){
+        scheduler.sh
+    }
+}
